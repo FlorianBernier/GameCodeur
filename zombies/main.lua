3ca5f48d3be44327ba2947538ea4f49feafe9ac6
Florian <florian.bernier@soait.fr>
2023-02-10 19:47:26 +0100

    create hero

--- zombies/main.lua
+++ zombies/main.lua
@@ -28,28 +28,71 @@ local function mousepressed_get_pos()
     end
 end
 --- --- --- --- --- --- --- --- --- --- --- --- ---
+local CONST = {speedHero = 100}
+
 local listSprite = {}
 local hero = {}
 
-function createSprite()
+function createSprite(pList, pType, pImgFileName, pFrame)
     local mySprite = {}
-    
+    mySprite.type = pType
+    mySprite.img = {}
+    mySprite.currentFrame = 1
+    for i = 1, pFrame do
+        local fileName = "images/"..pImgFileName..tostring(i)..".png"
+        print("Loading frame "..fileName)
+        mySprite.img[i] = love.graphics.newImage(fileName)
+    end
+
+    mySprite.x = 0
+    mySprite.y = 0
+    mySprite.w = mySprite.img[1]:getWidth()
+    mySprite.h = mySprite.img[1]:getHeight()
+
+    table.insert(pList, mySprite)
     return mySprite
 end
 
 love.load = function()
     load_full_screen()
-    ScreenWidth = love.graphics.getWidth()--larg(800)
-    ScreenHeight = love.graphics.getHeight()--haut(600)
+    ScreenWidth = love.graphics.getWidth()/2--larg(800)x
+    ScreenHeight = love.graphics.getHeight()/2--haut(600)y
 
-    hero = createSprite()
+    hero = createSprite(listSprite, "hero", "player_", 4)
+    hero.x = ScreenWidth / 2
+    hero.y = ScreenHeight / 2
 end
 
 love.update = function(dt)
+    for i, sprite in ipairs(listSprite) do
+        sprite.currentFrame = sprite.currentFrame + 9*dt
+        if sprite.currentFrame >= #sprite.img+1 then-- +1: du fait d'avoir utiliser math.floor(frame)
+            sprite.currentFrame = 1
+        end
+    end
+    if love.keyboard.isDown("left") and hero.x > 0 then
+        hero.x = hero.x - CONST.speedHero * dt
+    end
+    if love.keyboard.isDown("right") and hero.x < ScreenWidth then
+        hero.x = hero.x + CONST.speedHero * dt
+    end
+    if love.keyboard.isDown("up") and hero.y > 0 then
+        hero.y = hero.y - CONST.speedHero * dt
+    end
+    if love.keyboard.isDown("down") and hero.y < ScreenHeight then
+        hero.y = hero.y + CONST.speedHero * dt
+    end
 end
 
 love.draw = function()
     draw_full_screen()
+    love.graphics.push()
+    love.graphics.scale(2, 2)
+    for i, sprite in ipairs(listSprite) do
+        local frame = sprite.img[math.floor(sprite.currentFrame)]
+        love.graphics.draw(frame, sprite.x - (sprite.w/2), sprite.y - (sprite.h/2))
+    end
+    love.graphics.pop()
 end
 
 love.keypressed = function(key)
