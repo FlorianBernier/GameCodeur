6f2b635a92bb4490b70f8be26f71727451bd241d
Florian <florian.bernier@soait.fr>
2023-02-11 15:44:42 +0100

    create tp kirk 1ere passe

--- visualEffect/main.lua
+++ visualEffect/main.lua
@@ -29,43 +29,80 @@ local function mousepressed_get_pos()
 end
 --- --- --- --- --- --- --- --- --- --- --- --- ---
 
+local scaleZoom = 4
+
+local kirk = {}
+kirk.isBeam = true
+kirk.beamLevel = 1
+kirk.maxPercent = 100
+kirk.img = nil
+kirk.x = 0
+kirk.y = 0
+
+local sndTransporter = love.audio.newSource("sons/voyager.wav", "static")
+
+--- --- --- --- --- --- --- --- --- --- --- --- ---
+
+function createKirk()
+    kirk.img = love.graphics.newImage("images/kirk.png")
+    kirk.x = math.floor(ScreenWidth/2) - math.floor(kirk.img:getWidth()/2)
+    kirk.Y = math.floor(ScreenHeight/2) - math.floor(kirk.img:getHeight()/2)
+
+    love.audio.play(sndTransporter)
+end
+
 love.load = function()
     load_full_screen()
+    love.graphics.setBackgroundColor(0,0.9,0.9)
+    ScreenWidth = love.graphics.getWidth() / scaleZoom--larg
+    ScreenHeight = love.graphics.getHeight() / scaleZoom--haut
+    --- --- ---
+    createKirk()
+end
 
-    ScreenWidth = love.graphics.getWidth() --larg
-    ScreenHeight = love.graphics.getHeight() --haut
+
+function updateKirk(dt)
+    if kirk.isBeam then
+        local coef = 0.4 * 60 * dt
+        kirk.beamLevel = kirk.beamLevel + coef
+
+        if kirk.beamLevel >= kirk.maxPercent then
+            kirk.isBeam = false
+            kirk.beamLevel = 1
+        end
+        print(kirk.beamLevel)
+    end
 end
 
 love.update = function(dt)
+    updateKirk(dt)
+end
+
+
+function drawKirk()
+    if kirk.isBeam == false then
+        love.graphics.draw(kirk.img, kirk.x, kirk.Y)
+    else
+        --kirk en filigramme
+        love.graphics.setColor(1,1,1,1*(kirk.beamLevel)/(kirk.maxPercent))
+        love.graphics.draw(kirk.img, kirk.x, kirk.Y)
+    end
 end
 
 love.draw = function()
     draw_full_screen()
+    love.graphics.scale(scaleZoom,scaleZoom)
+    --- --- ---
+    drawKirk()
 end
+
 
 love.keypressed = function(key)
     keypressed_key(key)
     print(key)
 end
 
+
 love.mousepressed = function()
     mousepressed_get_pos()
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
