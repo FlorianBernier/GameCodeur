c14123ec51a3546dd54f957dbf4135220c347af7
Florian <florian.bernier@soait.fr>
2023-03-09 11:59:41 +0100

    base projet setting img/vidéo/snd IA

--- LuaLove2D/introMenu/base/intro.lua
+++ LuaLove2D/introMenu/base/intro.lua
@@ -4,28 +4,18 @@ local gameIntro = {}
 
 
 gameIntro.load = function()
-    Setting.load()
-    --- --- ---
 end
 
 gameIntro.update = function(dt)
-    Setting.update(dt)
-    --- --- ---
 end
 
 gameIntro.draw = function()
-    Setting.draw()
-    --- --- ---
 end
 
 gameIntro.keypressed = function(key)
-    Setting.keypressed(key)
-    --- --- ---
 end
 
 gameIntro.mousepressed = function(x, y, button)
-    Setting.mousepressed()
-    --- --- ---
 end
 
 
--- LuaLove2D/introMenu/base/menu.lua
+++ LuaLove2D/introMenu/base/menu.lua
@@ -4,30 +4,19 @@ local gameMenu = {}
 
 
 gameMenu.load = function()
-    Setting.load()
-    --- --- ---
 end
 
 gameMenu.update = function(dt)
-    Setting.update(dt)
-    --- --- ---
 end
 
 gameMenu.draw = function()
-    Setting.draw()
-    --- --- ---
 end
 
 gameMenu.keypressed = function(key)
-    Setting.keypressed(key)
-    --- --- ---
 end
 
 gameMenu.mousepressed = function(x, y, button)
-    Setting.mousepressed()
-    --- --- ---
 end
-
 
 
 return gameMenu
