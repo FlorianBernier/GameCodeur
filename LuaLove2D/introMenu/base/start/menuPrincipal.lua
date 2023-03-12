26ec648edd4d2338884e44f5056b1c8e56e5fb06
Florian <florian.bernier@soait.fr>
2023-03-12 19:17:43 +0100

    effect lettres menuPrincipale

--- LuaLove2D/introMenu/base/start/menuPrincipal.lua
+++ LuaLove2D/introMenu/base/start/menuPrincipal.lua
@@ -19,16 +19,66 @@ gameMenuPrincipal.btn = {
     {text = "SUPPLEMENTS", x=475, y=475, w=200, h=50, on = false}
 }
 local font = love.graphics.newFont(15)
+local spacePressed = false
+local spaceWidth = font:getWidth("")
 
 local movieW, movieH = gameMenuPrincipal.movieMenu:getDimensions()
 local introX = 800 / movieW
 local introY = 600 / movieH
 
 
-gameMenuPrincipal.load = function()
+local function loadBtn()
+    for i,button in ipairs(gameMenuPrincipal.btn) do
+        button.letters = {}
+        for j=1,#button.text do
+          table.insert(button.letters, {
+            x = math.random(love.graphics.getWidth()),
+            y = math.random(love.graphics.getHeight()),
+            targetX = button.x + button.w/2 - font:getWidth(button.text)/2 + (j-1)*spaceWidth + font:getWidth(button.text:sub(1,j))-font:getWidth(" ") + (button.text:sub(j,j) == "I" and 2 or 0),
+            targetY = button.y + button.h/2 - font:getHeight()/2,
+            letter = button.text:sub(j,j),
+            speed = math.random(50,100),
+            done = false
+          })
+        end
+    end
 end
 
+gameMenuPrincipal.load = function()
+    loadBtn()
+end
+
+
+local function updateBtn(dt)
+    if spacePressed then -- vérifier si la touche "space" a été enfoncée
+        for i,button in ipairs(gameMenuPrincipal.btn) do
+          for j,letter in ipairs(button.letters) do
+            if not letter.done then
+              local dx = letter.targetX - letter.x
+              local dy = letter.targetY - letter.y
+              local distance = math.sqrt(dx*dx + dy*dy)
+              local nx = dx/distance
+              local ny = dy/distance
+              local moveDistance = letter.speed * dt
+              if moveDistance > distance then
+                letter.x = letter.targetX
+                letter.y = letter.targetY
+                letter.done = true
+              else
+                letter.x = letter.x + nx * moveDistance
+                letter.y = letter.y + ny * moveDistance
+              end
+              if letter.letter == "I" then
+                letter.x = letter.x + 0.1
+                end
+            end
+          end
+        end
+      end
+  end
+
 gameMenuPrincipal.update = function(dt)
+    updateBtn(dt)
 end
 
 
@@ -42,7 +92,13 @@ local function drawMenuPrincipal()
             local v = gameMenuPrincipal.btn[i]
             love.graphics.rectangle("line", v.x, v.y, v.w, v.h)
             love.graphics.setFont(font)
-            love.graphics.printf(v.text, v.x, v.y + v.h/2 - font:getHeight()/2, v.w, "center")
+            for j,letter in ipairs(v.letters) do
+                if letter.letter == "I" then
+                    love.graphics.print(letter.letter, letter.x + 4, letter.y)
+                else
+                    love.graphics.print(letter.letter, letter.x, letter.y)
+                end
+            end
         end
     end
     if gameMenuPrincipal.on == false and gameMenuPrincipal.play == false then
@@ -69,6 +125,7 @@ local function drawMenu()
     end
 end
 
+
 gameMenuPrincipal.draw = function()
     drawMenuPrincipal()
     drawMenu()
@@ -76,6 +133,9 @@ end
 
 
 local function returnMenuPrincipal(key)
+    if key == "space" then
+        spacePressed = true
+    end
     if key == "escape" then
         gameMenuPrincipal.on = true
         gameMenuPrincipal.play = false
