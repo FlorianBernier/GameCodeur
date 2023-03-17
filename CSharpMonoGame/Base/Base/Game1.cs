8eae10ae995ce1cd1f70475ad6227c6d6db1edf8
FlorianBernier <florian.bernier@soait.fr>
2023-03-17 16:11:43 +0100

    base

--- CSharpMonoGame/Base/Base/Game1.cs
+++ CSharpMonoGame/Base/Base/Game1.cs
@@ -7,57 +7,46 @@ namespace Base
     // Classe principale du jeu
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
         }
 
-        // Fonction appelée une fois pour initialiser le jeu
         protected override void Initialize()
         {
-            // TODO: Ajoutez ici votre code d'initialisation
+            // TODO: Ajoutez ici votre code
 
             base.Initialize();
         }
 
-        // Fonction appelée une seule fois pour charger le contenu du jeu
         protected override void LoadContent()
         {
-            // Crée un nouveau SpriteBatch, qui sera utilisé pour afficher des images (textures)
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: Ajoutez ici votre code qui chargera le contenu du jeu
-        }
-
-        // Fonction appelée une fois pour décharger le contenu du jeu (hors ContentManager)
-        protected override void UnloadContent()
-        {
+            // TODO: Ajoutez ici votre code 
 
         }
 
-        // Fonction appelée 60x par seconde pour mettre à jour l'état du jeu
-        // Reçoit "gametime" qui contient le temps écoulé depuis le dernier update
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Ajoutez le code de mise à jour ici
+            // TODO: Ajoutez ici votre code
 
             base.Update(gameTime);
         }
 
-        // Fonction appelée aussi souvent que possible (jusqu'à 60x par seconde) pour afficher le jeu
-        // Reçoit "gametime" qui contient le temps écoulé depuis le dernier update
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Ajouter le code d'affichage ici
+            // TODO: Ajoutez ici votre code
 
             base.Draw(gameTime);
         }
