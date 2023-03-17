b267bea492ed6c19b5d0753d7448d485638bbaef
FlorianBernier <florian.bernier@soait.fr>
2023-03-17 18:06:28 +0100

    img slime

--- CSharpMonoGame/Image/Image/Game1.cs
+++ CSharpMonoGame/Image/Image/Game1.cs
@@ -10,6 +10,11 @@ namespace Image
         private SpriteBatch _spriteBatch;
         Texture2D img;
         Vector2 position;
+        int speed;
+
+        Texture2D img2;
+        Vector2 position2;
+        int speed2;
 
         public Game1()
         {
@@ -21,7 +26,10 @@ namespace Image
         protected override void Initialize()
         {
             // TODO: Ajoutez ici votre code
-            position = new Vector2 (100, 100);
+            position = new Vector2 (100, 0);
+            position2 = new Vector2(100, 200);
+            speed = 5;
+            speed2 = 5;
 
             base.Initialize();
         }
@@ -32,6 +40,7 @@ namespace Image
 
             // TODO: Ajoutez ici votre code
             img = this.Content.Load<Texture2D>("personnage");
+            img2 = this.Content.Load<Texture2D>("slimePurple");
         }
 
         protected override void Update(GameTime gameTime)
@@ -40,6 +49,26 @@ namespace Image
                 Exit();
 
             // TODO: Ajoutez ici votre code
+            position.X = position.X + speed;
+            if (position.X + img.Width > GraphicsDevice.Viewport.Width)
+            {
+                speed = 0 - speed;
+            }
+            if (position.X < 0)
+            {
+                speed = 0 - speed;
+            }
+
+            position2.X = position2.X + speed2;
+            if (position2.X + img2.Width > GraphicsDevice.Viewport.Width)
+            {
+                speed2 = 0 - speed2;
+            }
+            if (position2.X < 0)
+            {
+                speed2 = 0 - speed2;
+            }
+
 
             base.Update(gameTime);
         }
@@ -51,6 +80,11 @@ namespace Image
             // TODO: Ajoutez ici votre code
             _spriteBatch.Begin();
             _spriteBatch.Draw(img, position, Color.White);
+            SpriteEffects effect;
+            effect = SpriteEffects.None;
+            if (speed2 > 0)
+                effect = SpriteEffects.FlipHorizontally;
+            _spriteBatch.Draw(img2, position2, null, Color.White, 0, Vector2.Zero, 1.0f, effect, 0);
             _spriteBatch.End();
 
             base.Draw(gameTime);
