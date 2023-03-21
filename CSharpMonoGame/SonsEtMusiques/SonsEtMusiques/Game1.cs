db27f3c6b1116f77ec962c00bc794d4ee22d193a
FlorianBernier <florian.bernier@soait.fr>
2023-03-21 10:53:03 +0100

    init SntAndMusic

---
+++ CSharpMonoGame/SonsEtMusiques/SonsEtMusiques/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SonsEtMusiques
+{
+    // Classe principale du jeu
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Ajoutez ici votre code
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: Ajoutez ici votre code
+            //Song myMusic = Content.Load<Song>("cool");
+            //MediaPlayer.IsRepeating = true;
+            //MediaPlayer.Play(myMusic);
+
+            SoundEffect mySound = Content.Load<SoundEffect>("sfx_movement_jump13");
+            mySound.Play();
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Ajoutez ici votre code
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Ajoutez ici votre code
+
+            base.Draw(gameTime);
+        }
+    }
+}
