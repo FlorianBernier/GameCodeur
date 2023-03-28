502d48faecc44561e83a6c7d1bffd3af894e3be7
FlorianBernier <florian.bernier@soait.fr>
2023-03-28 11:15:32 +0200

    init project and load img

---
+++ CSharpMonoGame/3DIsometrique/3DIsometrique/Main.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace _3DIsometrique
+{
+    public class Main : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        List<Texture2D> lstTextures2D;
+        List<Texture2D> lstTextures3D;
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            lstTextures2D = new List<Texture2D>();
+            lstTextures3D = new List<Texture2D>();
+
+            Texture2D tx;
+            tx = Content.Load<Texture2D>("stone2D");
+            lstTextures2D.Add(tx);
+            tx = Content.Load<Texture2D>("dirt2D");
+            lstTextures2D.Add(tx);
+
+
+            tx = Content.Load<Texture2D>("stone3D");
+            lstTextures2D.Add(tx);
+            tx = Content.Load<Texture2D>("dirt3D");
+            lstTextures2D.Add(tx);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ CSharpMonoGame/3DIsometrique/3DIsometrique/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new _3DIsometrique.Main();
+game.Run();
