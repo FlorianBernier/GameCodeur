de9d4bf04b1fffb6d63a66bd0b671340e59deadd
FlorianBernier <florian.bernier@soait.fr>
2023-03-29 17:15:48 +0200

    init project Tilled and create map

---
+++ CSharpMonoGame/Tilled/Tilled/Main.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TiledSharp;
+
+namespace Tilled
+{
+    public class Main : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // Map
+        TmxMap map;
+        Texture2D tileset;
+        int tileWidth;
+        int tileHeight;
+        int mapWidth;
+        int mapHeight;
+        int tilesetColumn;
+        int tilesetLine;
+
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            map = new TmxMap("Content/map.tmx");
+            tileset = Content.Load<Texture2D>(map.Tilesets[0].Name.ToString());
+
+            tileWidth = map.Tilesets[0].TileWidth;
+            tileHeight = map.Tilesets[0].TileHeight;
+
+            mapWidth = map.Width;
+            mapHeight = map.Height;
+
+            tilesetColumn = tileset.Width / tileWidth;
+            tilesetLine = tileset.Height / tileHeight;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ CSharpMonoGame/Tilled/Tilled/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Tilled.Main();
+game.Run();
