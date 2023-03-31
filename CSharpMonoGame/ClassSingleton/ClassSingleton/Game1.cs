23453dae9ed46c717b3f75ca5161f1efd9502bb0
FlorianBernier <florian.bernier@soait.fr>
2023-03-31 11:20:55 +0200

    Singleton

---
+++ CSharpMonoGame/ClassSingleton/ClassSingleton/Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace ClassSingleton
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            MaClass instance1 = MaClass.GetInstance();
+            MaClass instance2 = MaClass.GetInstance();
+
+            Debug.WriteLine("Assignation de Soanara dans instance1");
+            instance1.Value = "Soanara";
+
+            Debug.WriteLine($"instance1 = {instance1.Value}");
+            Debug.WriteLine($"instance2 = {instance1.Value}");
+
+
+            Debug.WriteLine("Assignation de Héresie dans instance1");
+            instance1.Value = "Héresie";
+
+            Debug.WriteLine($"instance1 = {instance1.Value}");
+            Debug.WriteLine($"instance2 = {instance1.Value}");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ CSharpMonoGame/ClassSingleton/ClassSingleton/MaClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Créé une instance unique (permet d'acceder a des fonction unique)
+namespace ClassSingleton
+{
+    public class MaClass
+    {
+        public string Value { get; set; }
+        private static MaClass instance;
+
+        private MaClass() 
+        {
+        
+        }
+
+        public static MaClass GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MaClass();
+            }
+            return instance;
+        }
+    }
+}
