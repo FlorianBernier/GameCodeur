3d58f9f4dfe0fec602ac5e1cda15bd8ae179bd82
FlorianBernier <florian.bernier@soait.fr>
2023-03-31 13:25:28 +0200

    ClassState

---
+++ CSharpMonoGame/ClassState/ClassState/Zombie.cs
@@ -0,0 +1,34 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassState
+{
+    public enum ZombieStimuli
+    {
+        SeeNothing,
+        SeeHuman,
+        OnHuman
+    }
+    public class Zombie
+    {
+        public ZombieStimuli WhatHappen { get; set; } // Simule un stimuli 
+        private IZombieState _state;
+
+
+
+        public void ChangeState(IZombieState newState)
+        {
+            _state = newState;
+        }
+
+
+        public void DoSomething()
+        {
+            _state.DoSomething();
+        }
+    }
+}
