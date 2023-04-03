0ea5e230132979c349a2a8a54a1c951d1a24b4f9
FlorianBernier <florian.bernier@soait.fr>
2023-04-03 14:20:35 +0200

    MoveCamera and Base/fullscreen/

--- CSharpMonoGame/Setting/Setting/Main.cs
+++ CSharpMonoGame/Setting/Setting/Main.cs
@@ -4,6 +4,30 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Setting
 {
+    public class Camera2D
+    {
+        public Vector2 Position { get; set; }
+        public float Zoom { get; set; }
+        public float Rotation { get; set; }
+
+        private readonly Viewport _viewport;
+
+        public Camera2D(Viewport viewport)
+        {
+            _viewport = viewport;
+            Zoom = 1f;
+            Rotation = 0f;
+            Position = Vector2.Zero;
+        }
+
+        public Matrix GetViewMatrix()
+        {
+            return Matrix.CreateTranslation(new Vector3(-Position, 0f)) *
+                   Matrix.CreateRotationZ(Rotation) *
+                   Matrix.CreateScale(Zoom) *
+                   Matrix.CreateTranslation(new Vector3(_viewport.Width * 0.5f, _viewport.Height * 0.5f, 0f));
+        }
+    }
     public class Main : Game
     {
         public GraphicsDeviceManager graphics;
@@ -12,6 +36,8 @@ namespace Setting
         public BaseSetting mySetting;
         Texture2D imgTemplateBG;
 
+        private Camera2D _camera;
+        private float _cameraSpeed = 0.1f;
 
         public Main()
         {
@@ -27,6 +53,7 @@ namespace Setting
             // TODO: Ajoutez ici votre code
 
             base.Initialize();
+            _camera = new Camera2D(GraphicsDevice.Viewport);
         }
 
         protected override void LoadContent()
@@ -49,6 +76,13 @@ namespace Setting
             mySetting.Update(gameTime);
             // TODO: Ajoutez ici votre code
 
+            var mouseState = Mouse.GetState();
+
+            if (mouseState.MiddleButton == ButtonState.Pressed)
+            {
+                _camera.Position += new Vector2((mouseState.X - graphics.PreferredBackBufferWidth / 2) * _cameraSpeed,
+                                              (mouseState.Y - graphics.PreferredBackBufferHeight / 2) * _cameraSpeed);
+            }
             base.Update(gameTime);
         }
 
@@ -57,7 +91,7 @@ namespace Setting
             mySetting.Base(gameTime);
 
             // TODO: Ajoutez ici votre
-            spriteBatch.Begin();
+            spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix());
 
             spriteBatch.Draw(imgTemplateBG, new Vector2(0, 0), null, Color.White);
 
