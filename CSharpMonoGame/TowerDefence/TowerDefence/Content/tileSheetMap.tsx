f8a9f5f64f99258955e5a3827ec2392320806112
FlorianBernier <florian.bernier@soait.fr>
2023-04-04 11:55:58 +0200

    Init tower defance

---
+++ CSharpMonoGame/TowerDefence/TowerDefence/Content/tileSheetMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="tileSheetMap" tilewidth="32" tileheight="32" tilecount="198" columns="9">
+ <image source="tileSheetMap.png" width="288" height="704"/>
+</tileset>
---
+++ CSharpMonoGame/TowerDefence/TowerDefence/Main.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Configuration;
+
+namespace TowerDefence
+{
+    public class Main : Game
+    {
+        public GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
+
+        // Setting
+        public SettingBase _settingBase;
+        public FullScreen _fullScreen;
+        public MoveCamera _moveCamera;
+        
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // Setting
+            _settingBase = new SettingBase(this);
+            _fullScreen = new FullScreen(this);
+            _moveCamera = new MoveCamera(this);
+
+        }
+
+        protected override void Initialize()
+        {
+            _fullScreen.Initialize();
+            _moveCamera.Initialize();
+            // TODO: Ajoutez ici votre code
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: Ajoutez ici votre code
+            
+        }
+
+        protected override void UnloadContent()
+        {
+            _fullScreen.UnloadContent();
+            // TODO: Ajoutez ici votre code
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _fullScreen.Update(gameTime);
+            _moveCamera.Update(gameTime);
+            // TODO: Ajoutez ici votre code
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _settingBase.Draw(gameTime);
+            _fullScreen.DrawSet(gameTime);
+            // TODO: Ajoutez ici votre
+
+            spriteBatch.Begin(transformMatrix: _moveCamera._camera.GetViewMatrix());
+
+            
+
+            spriteBatch.End();
+
+
+            _fullScreen.Draw(gameTime);
+            base.Draw(gameTime);
+        }
+    }
+}
