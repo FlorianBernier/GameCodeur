f937297beb1b590cae8f1feee714d89e94b33563
FlorianBernier <florian.bernier@soait.fr>
2023-04-10 11:59:14 +0200

    Organisation de code 1 2 et 3

--- CSharpMonoGame/ClassObserver/ClassObserver/Client.cs
+++ CSharpMonoGame/ClassObserver/ClassObserver/Client.cs
@@ -24,4 +24,106 @@ namespace ClassObserver
             Debug.WriteLine($"Client : {Name} Le magasin a recu du stock.");
         }
     }
+
+
+
+
+    public class Tower
+    {
+        public string name;
+        public int qte;
+        public bool cdTir;
+        public bool startCd;
+
+        public void attack()
+        {
+
+        }
+    }
+
+
+
+    public class TowerFilter
+    {
+        List<Tower> liste;
+        public TowerFilter(List<Tower> liste)
+        {
+            this.liste = liste;
+
+
+        }
+
+        public TowerFilter NameContain(string name)
+        {
+
+            liste = liste.FindAll(tower => tower.name.Contains(name));
+            return this;
+        }
+        public TowerFilter QteGt(int qte)
+        {
+            liste = liste.FindAll(tower => tower.qte > qte);
+            return this;
+        }
+
+        public TowerFilter QteLt(int qte)
+        {
+            liste = liste.FindAll(tower => tower.qte < qte);
+            return this;
+        }
+
+        public TowerFilter CooldownShootIsUp()
+        {
+            liste = liste.FindAll(tower => tower.cdTir);
+            return this;
+        }
+        public TowerFilter Attack()
+        {
+            liste.ForEach(tower => tower.attack());
+            return this;
+        }
+
+        public TowerFilter RestartCooldown()
+        {
+            liste.ForEach(tower => tower.startCd = true);
+            return this;
+        }
+
+
+
+        public List<Tower> Build()
+        {
+            return liste;
+        }
+
+
+
+        public void main()
+        {
+            List<Tower> liste = new List<Tower>
+            {
+                new Tower(),
+                new Tower(),
+                new Tower(),
+                new Tower(),
+                new Tower(),
+            };
+
+            new TowerFilter(liste)
+                .all()
+                    .CooldownShootIsUp()
+                        .Attack()
+                        .RestartCooldown()
+                .all()
+                    .hasBeenDestroyed()
+                        .Delete()
+                .all()
+                    .AddAtk(5)
+                .all()
+                    .type(Type.FIRE)
+                        .IsParalized()
+                .all()
+
+        }
+
+    }
 }
--- CSharpMonoGame/ClassObserver/ClassObserver/Magasin.cs
+++ CSharpMonoGame/ClassObserver/ClassObserver/Magasin.cs
@@ -64,4 +64,193 @@ namespace ClassObserver
             }
         }
     }
+    
+
+    public interface ITower
+    {
+        event Upgrade upgradeDelegue;
+
+        public void attack();
+
+        public delegate void Upgrade();
+    }
+
+
+
+    public class FireTower : ITower
+    {
+
+
+        public FireTower(String type)
+        {
+            switch (type)
+            {
+                case "arme 1":
+                    upgradeDelegue = towerFeu1;
+                    break;
+                case "arme 2":
+                    upgradeDelegue = towerFeu2;
+                    break;
+                case "arme 3":
+                    upgradeDelegue = towerFeu3;
+                    break;
+            }
+        }
+
+        public event ITower.Upgrade upgradeDelegue;
+
+        public void attack()
+        {
+        }
+
+        public void towerFeu1()
+        {
+        }
+
+        public void towerFeu2()
+        {
+        }
+        public void towerFeu3()
+        {
+        }
+
+
+    }
+
+
+    public class IceTower : ITower
+    {
+        public event ITower.Upgrade upgradeDelegue;
+
+        public void attack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class A
+    {
+        public void ezaeza()
+        {
+            List<ITower> list = new List<ITower>{
+                new IceTower(),
+                new FireTower("arme 2"),
+            };
+
+
+            list.ForEach(tower => tower.attack());
+
+            list.ForEach(tower => tower.Equals(null));
+
+        }
+    }
+
 }
+
+
+
+
+/*
+public class PlayerBuilder
+{
+
+    private string name;
+    private string description;
+    private Vector2 pos;
+    private int level;
+    private long life;
+    private List<string> names;
+
+
+    public PlayerBuilder()
+    {
+        List<string> names = new List<string>();
+    }
+
+    public PlayerBuilder addName(string name)
+    {
+        names.Add(name);
+        return this;
+    }
+
+    public PlayerBuilder Name(string name)
+    {
+        this.name = name;
+        return this;
+    }
+    public PlayerBuilder Description(string description)
+    {
+        this.description = description;
+        return this;
+    }
+    public PlayerBuilder Pos(Vector2 pos)
+    {
+        this.pos = pos;
+        return this;
+    }
+    public PlayerBuilder Level(int level)
+    {
+        this.level = level;
+        return this;
+    }
+
+    public PlayerBuilder Life(int life)
+    {
+        this.life = life;
+        return this;
+    }
+
+    public Player Build()
+    {
+        return new Player(this.name, this.description, this.pos, this.level, this.life);
+    }
+
+    public class Player
+    {
+        private string name;
+        private string description;
+        private Vector2 pos;
+        private int level;
+        private long life;
+        private List<string> names;
+
+        public Player(string name, string description, Vector2 pos, int level, long life)
+        {
+            this.name = name;
+            this.description = description;
+            this.pos = pos;
+            this.level = level;
+            this.life = life;
+        }
+    }
+
+    public void main()
+    {
+        Player monPlayer = (new PlayerBuilder())
+            .Level(5)
+            .addName("Flo")
+            .addName("Allan")
+            .addName("Aurore")
+            .Description("Coucou new player")
+            .Name("Soa")
+            .Build();
+    }
+*/
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
