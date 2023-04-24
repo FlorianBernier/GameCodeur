ba88a6b324b2ab44b615907dec73a6f0ba494f66
FlorianBernier <florian.bernier@soait.fr>
2023-04-24 21:33:38 +0200

    méthode singleton

--- CSharpMonoGame/ClassSingleton/ClassSingleton/Game1.cs
+++ CSharpMonoGame/ClassSingleton/ClassSingleton/Game1.cs
@@ -9,7 +9,7 @@ namespace ClassSingleton
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
