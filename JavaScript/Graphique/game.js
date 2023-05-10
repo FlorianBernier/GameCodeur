f7db35045bb40fe9eef84776e06ae55320b32a02
FlorianBernier <florian.bernier@soait.fr>
2023-05-10 16:37:17 +0200

    load / update / draw

--- JavaScript/Graphique/game.js
+++ JavaScript/Graphique/game.js
@@ -1,19 +1,25 @@
 
 
-
+let img;
+let img2;
 
 function load()
 {
-
+    img = new Sprite("images/hero.png", 100, 100);
+    img2 = new Sprite("images/hero.png", 0, 0);
+       
 }
 
 function update()
 {
-    
+    img.x++
+    img.y++
 }
 
 function draw(pCtx)
 {
+    img.draw(pCtx)
+    img2.draw(pCtx)
 
 }
 
